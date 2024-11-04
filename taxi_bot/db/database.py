0eb96d1830e7ee9e1a5d4a14ac556bf6caa9cf14
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2024-11-04 07:07:40 +0300

    Update database.py

--- taxi_bot/db/database.py
+++ taxi_bot/db/database.py
@@ -144,6 +144,5 @@ async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-asyncio.run(create_tables())
 
 AsyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
