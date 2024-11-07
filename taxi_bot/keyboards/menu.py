dbf72ca3c7f8487d3f13808cec0bee1a3a1bfd01
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2024-11-07 07:44:36 +0300

    Update menu.py

--- taxi_bot/keyboards/menu.py
+++ taxi_bot/keyboards/menu.py
@@ -4,7 +4,7 @@ from aiogram.types import BotCommand
 
 # Таъин кардани фармонҳо барои ронанда
 async def set_menu_commands(bot: Bot):
-    from bot import bot
+    from bot_file import bot
     
     commands = [
             BotCommand(command="how_to_use_bot", description="Истифодабарии бот"),
@@ -12,11 +12,11 @@ async def set_menu_commands(bot: Bot):
             BotCommand(command="menu", description="Меню"),
             BotCommand(command="order_a_taxi", description="Фармоиши таксӣ"),
             BotCommand(command="my_drivers", description="Ронандаи ман"),
-            BotCommand(command="client_account", description="Аккаунт барои клиент"),
-            BotCommand(command="my_posts", description="Постҳои ронанда"),
-            BotCommand(command="my_clients", description="Клиентҳои ронанда"),
+            BotCommand(command="my_posts", description="Постҳои ман"),
+            BotCommand(command="my_clients", description="Клиентҳои ман"),
             BotCommand(command="new_trip", description="Сафари нав ба қайд гирифтан"),
-            BotCommand(command="account", description="Аккаунт барои ронанда")
+            BotCommand(command="account", description="Аккаунт ҳамчун ронанда")
+            BotCommand(command="client_account", description="Аккаунт ҳамчун клиент"),
             
        ]
     await bot.set_my_commands(commands)
