b49c2897e12c35fe9bb6b88288d1f5e61815f075
jalilovdavlatshoh49 <143313280+jalilovdavlatshoh49@users.noreply.github.com>
2024-11-07 06:05:17 +0300

    Update bot.py

--- taxi_bot/bot.py
+++ taxi_bot/bot.py
@@ -3,6 +3,9 @@ import logging
 from aiogram import Bot, Dispatcher, Router, types
 from aiogram.types import BotCommand
 from handlers.start_handler import start_router
+from handlers.client_handler import start_router as client_router
+from handlers.driver_handler import start_router as driver_router
+from handlers.inline_menu_handler import start_router as inline_menu_router
 from aiogram.fsm.storage.memory import MemoryStorage
 from keyboards.menu import set_menu_commands
 from db.database import create_tables
@@ -21,6 +24,9 @@ async def main():
     await set_menu_commands(bot)
     
     dp.include_router(start_router)
+    dp.include_router(client_router)
+    dp.include_router(driver_router)
+    dp.include_router(inline_menu_router)
     
     # Оғози боти Telegram
     await bot.delete_webhook(drop_pending_updates=True)
